f887597a88750c5c03c246f01e32f0922e3d548e
Adam Hao <haozg44@outlook.com>
2024-09-21 00:29:07 +0800

    finish forth exercise

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import BlogHero from '@/components/BlogHero';
+import CodeSnippet from '@/components/CodeSnippet';
 
 import { loadBlogPost } from '@/helpers/file-helpers';
 
@@ -25,7 +26,12 @@ async function BlogPost({ params: { postSlug } }) {
 		<article className={styles.wrapper}>
 			<BlogHero title='Example post!' publishedOn={new Date()} />
 			<div className={styles.page}>
-				<MDXRemote source={postContent} />
+				<MDXRemote
+					source={postContent}
+					components={{
+						pre: CodeSnippet,
+					}}
+				/>
 			</div>
 		</article>
 	);
--- src/components/CodeSnippet/CodeSnippet.js
+++ src/components/CodeSnippet/CodeSnippet.js
@@ -4,14 +4,11 @@ import { Code } from 'bright';
 import theme from './theme';
 import styles from './CodeSnippet.module.css';
 
+Code.lineNumbers = true;
+Code.theme = theme;
+
 function CodeSnippet(props) {
-  return (
-    <Code
-      {...props}
-      theme={theme}
-      className={styles.wrapper}
-    />
-  );
+	return <Code {...props} className={styles.wrapper} />;
 }
 
 export default CodeSnippet;
